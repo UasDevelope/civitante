d5ef5051aa9e7edc2726306a28a568cc7a74964e
zohaib521321 <khanzohaib3231@gmail.com>
2025-02-03 12:40:04 +0500

    Stripe init

--- android/app/src/main/kotlin/com/example/civitante/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/civitante/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.app.civitante
-
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
