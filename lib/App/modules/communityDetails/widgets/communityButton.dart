275aae0d0a68bfbc546ace9cd96dc7b9d54b3027
zohaib521321 <khanzohaib3231@gmail.com>
2025-03-13 05:19:55 +0500

    Refactor: Update community button widget for improved usability
    The community button widget has been updated to improve its usability:
    
    -   **Tooltip:** Added a tooltip to the button to provide clarity on the button's function when the user long-presses it.
    -   **Larger touch target:** Increased the size of the touch target to 48x48 for better touchability.
    -   **IconButton:** Replaced GestureDetector with IconButton.
    -   **Padding**: Added padding for visual spacing.
    -   **Icon size:** Updated icon size to 24.

--- lib/App/modules/communityDetails/widgets/communityButton.dart
+++ lib/App/modules/communityDetails/widgets/communityButton.dart
@@ -4,15 +4,29 @@ import 'package:flutter/material.dart';
 
 import '../../../utilse/widgets.dart';
 
-Widget buildActionCommunityButton(String assetPath, VoidCallback onTap) {
-  return SizedBox(
-    height: 30,
-    width: 30,
-    child: GestureDetector(
-      onTap: onTap,
-      child: Image.asset(
-        assetPath,
-        color: Colors.black,
+Widget buildActionCommunityButton({
+  required String assetPath,
+  required VoidCallback onTap,
+  required String tooltip,
+  double height=48,
+  double width=48
+}) {
+  return Tooltip(
+    message: tooltip, // Adds clarity on long press
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: SizedBox(
+        height: height, // Larger touch target
+        width: width,
+        child: IconButton(
+          onPressed: onTap,
+          icon: Image.asset(
+            assetPath,
+            color: Colors.black,
+            height: 24, // Keep icon size reasonable
+          ),
+          tooltip: tooltip, // Redundant with Tooltip widget, but good practice
+        ),
       ),
     ),
   );
