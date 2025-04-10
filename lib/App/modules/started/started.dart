9c14f13e4927c57bf84ae8249db4abf4bdb625ea
zohaib521321 <khanzohaib3231@gmail.com>
2025-04-10 12:33:38 +0500

    Update `emoji_picker_flutter` version to `4.3.0` and `shimmer` to `3.0.0`.

--- lib/App/modules/started/started.dart
+++ lib/App/modules/started/started.dart
@@ -9,49 +9,54 @@ class StartedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: screenWidth,
-          height: screenHeight,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(AppImages.splash),
-              fit: BoxFit.cover,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.splash),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            const Spacer(flex: 4),
+            AppText(
+              text: AppStrings.started,
+              color: AppColors.white,
+              fontSize: 30,
+              fontWeight: FontWeight.w400,
+              textAlign: TextAlign.center,
             ),
-          ),
-          child: Column(
-            children: [
-              const Spacer(flex: 4),
-              AppText(
-                text: AppStrings.started,
-                color: AppColors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w400,
-                textAlign: TextAlign.center,
+            const Spacer(flex: 1),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: AppButton(
+                useGradient: true,
+                text: AppStrings.getStarted,
+                height: 60.0,
+                width: double.infinity,
+                color: Colors.blue,
+                radius: 30,
+                onPressed: () {
+                  // NotificationUtil().showNotification(
+                  //   RemoteMessage(
+                  //     notification: RemoteNotification(
+                  //       apple: AppleNotification(
+                  //           sound: AppleNotificationSound(
+                  //               critical: true, name: "sound.caf")),
+                  //       title: "Test Notification",
+                  //       body: "This is a local test notification.",
+                  //     ),
+                  //     data: {"key": "value", "notificationType": "message"},
+                  //   ),
+                  // );
+                  log('Redirecting to LoginScreen --> ${AppRoutes.login}');
+                  Get.toNamed(AppRoutes.login);
+                },
               ),
-              const Spacer(flex: 2),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: AppButton(
-                  useGradient: true,
-                  text: AppStrings.getStarted,
-                  height: 60.0,
-                  width: double.infinity,
-                  color: Colors.blue,
-                  radius: 30,
-                  onPressed: () {
-                    log('Redirecting to LoginScreen --> ${AppRoutes.login}');
-                    Get.toNamed(AppRoutes.login);
-                  },
-                ),
-              ),
-              const Spacer(flex: 1),
-            ],
-          ),
+            ),
+            const Spacer(flex: 1),
+          ],
         ),
       ),
     );
